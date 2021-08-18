625386a901b35ce731708423d3d275bd46617ea8
Knute Lingaard <Knute.Lingaard@sifive.com>
2021-08-18 17:28:27 -0500

    Removed more example code; updated README

--- core/Core.hpp
+++ core/Core.hpp
@@ -4,17 +4,16 @@
 
 #include <string>
 
-#include "sparta/simulation/Clock.hpp"
 #include "sparta/simulation/Unit.hpp"
 #include "sparta/simulation/TreeNode.hpp"
 #include "sparta/functional/RegisterSet.hpp"
 #include "sparta/simulation/ParameterSet.hpp"
-#include "sparta/statistics/Counter.hpp"
-#include "sparta/events/Event.hpp"
-
 
 namespace core_example
 {
+    // In Olympia the Core class is just a placeholder and a
+    // full-blown unit.  In reality, it can just be a simple
+    // sparta::TreeNode
     class Core : public sparta::Unit
     {
     public:
@@ -25,19 +24,8 @@ namespace core_example
         public:
             CoreParameterSet(sparta::TreeNode* n) :
                 sparta::ParameterSet(n)
-            {
-                foo.addDependentValidationCallback([](std::string& s, const sparta::TreeNode*){return s.size() < 10;},
-                                                   "Length must be < 10");
-                ctr_incr_period.addDependentValidationCallback([](uint64_t& v, const sparta::TreeNode*){return v > 0;},
-                                                   "Counter increment period must be > 0");
-                ctr_incr_amount.addDependentValidationCallback([](uint32_t& v, const sparta::TreeNode*){return v > 0;},
-                                                   "Counter incrementor must be > 0");
-            }
+            { }
 
-            PARAMETER(std::string, foo, "default", "test parameter")
-            PARAMETER(uint64_t, ctr_incr_period, 1000, "Period of each 'counter_foo' counter update. Must be > 0")
-            PARAMETER(uint32_t, ctr_incr_amount, 1, "Value to increment the counter 'counter_foo' each counter update period. Must be > 0")
-            PARAMETER(std::vector<std::string>, contents, {}, "Content???")
         };
 
         //! \brief Name of this resource. Required by sparta::UnitFactory
@@ -56,25 +44,9 @@ namespace core_example
          * type, NOT sparta::ParameterSet.
          * \param ports Fully configured ports for this component.
          */
-        Core(sparta::TreeNode * node,
-             const CoreParameterSet * params); // Temporary type
+        Core(sparta::TreeNode * node, const CoreParameterSet * params);
 
         ~Core() {}
 
-
-    private:
-
-        /*!
-         * \brief Increments the example foo counter in this core
-         */
-        void incrementCounter_();
-
-        std::vector<Resource *> resources_;
-        std::unique_ptr<sparta::RegisterSet> regs_; //!< RegisterSet node
-        sparta::Counter* fooctr_; //!< Foo counter example (points to counter owned by ctrs_)
-        sparta::Event<> counter_incr_event_; //!< Callback for incrementing counter
-        uint64_t counter_incr_period_; //!< Period of counter increments (from Parameters)
-        uint64_t counter_incr_amount_; //!< Amount of each counter increment (from Parameters)
     };
 }
-
